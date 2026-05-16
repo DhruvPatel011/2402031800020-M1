ff0aea225016c56aecb485c97916cf01642eb850
Patel Dhruv <dhruvpatel5471@gmail.com>
2026-05-16 11:09:05 +0530

    Add PR-3,4

--- Day6/day6.js
+++ Day6/day6.js
@@ -1,14 +1,26 @@
 //PR-1: Use Arrow Function
 document.write("<h3>1. Use Arrow Function</h3>");
-let a = () => document.write("<h4>Arrow</h4>");
+let a = () => document.write("Arrow");
 a();
-document.write("<br>")
+document.write("<br><br>");
 
 //PR-2: Normal Function
 document.write("<h3>2. Normal Function</h3>");
 let b = function(){
-    document.write("<h4>Normal</h4>");
+    document.write("Normal");
 }
 b();
-document.write("<br>")
+document.write("<br><br>");
+
+//PR-3: Math Method
+document.write("<h3>3. Math Method</h3>");
+document.write(Math.E);
+document.write("<br><br>");
+
+//PR-4: Date Object
+const d = new Date();
+document.write("<h3>4. Date Object</h3>");
+document.write(d);
+
+
 
