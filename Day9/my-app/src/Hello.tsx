c197f62b56e4b55aad8a0254e9ca722032c0f700
Patel Dhruv <dhruvpatel5471@gmail.com>
2026-05-21 11:33:02 +0530

    Updated

--- Day9/my-app/src/Hello.tsx
+++ Day9/my-app/src/Hello.tsx
@@ -1,7 +1,29 @@
 function Hello(){
+
+    const getname = (yourname) => {
+        return yourname
+    }
+    const name = "Dhruv"
+    const name1 = "Patel"
+
+    function handleClick(){
+      alert("Clicked")
+    }
+
+
     return(
         <>
-            <h3 >It's inside <code>Hello.tsx</code> File... !</h3>
+            <h2 >It's inside <code>Hello.tsx</code> File... ! 
+            <br /><br />
+                Hello <code>{getname(name)}</code>...!
+            <br />
+                Bye <code>{getname(name1)}</code>...!
+            <br /><br />
+                <button onClick={handleClick}>Click</button>
+            <br />
+                <button onClick={() => alert("Arrow")}>Arrow</button>
+            
+            </h2>
         </>
     )
 }
